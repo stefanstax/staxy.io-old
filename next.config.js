4711a1e76d3e533d9f9748bc9537f57e48e41441
Stefan Stäx <stefanstax@Stefans-MacBook-Air.local>
2023-06-23 00:51:01 +0200

    WIP Removal of .txt for subpages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: { unoptimized: true },
   experimental: {
     mdxRs: true,
   },
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
 };
 
 const withMDX = require("@next/mdx")();
